d1fe900f922b44853762efb03d032cc3e4bcf4aa
刘清营 <wb-lqy644729@koubei.com>
2021-04-17 20:45:00 +0800

    初始化代码

---
+++ src/pages/About.vue
@@ -0,0 +1,29 @@
+<template>
+  <main-layout>
+    <p class="abc">About page {{content}}</p>
+  </main-layout>
+</template>
+
+<script>
+  import MainLayout from '../layouts/Main.vue'
+  // import less from './style/index.less'
+  export default {
+    data() {
+      return {
+        content: "11ss111",
+      }
+    },
+    components: {
+      MainLayout
+    }
+  }
+</script>
+<style lang="less" >
+ @base-color: #39c;
+.abc {
+  margin: 20px;
+  background: @base-color;
+}
+</style>
+  
+
